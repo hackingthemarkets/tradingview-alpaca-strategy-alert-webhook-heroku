09dbba32593a7656fdb5fb422639eb0be4943611
SkyShadex <116922168+SkyShadex@users.noreply.github.com>
2023-06-01 16:01:13 -0400

    Update orderlogic.py

--- components/orderlogic.py
+++ components/orderlogic.py
@@ -3,14 +3,18 @@ import alpaca_trade_api as tradeapi
 
 import config, json, requests
 
-api = tradeapi.REST(config.API_KEY, config.API_SECRET, paper=True)
+# Declare some variables.
+var bool canTrade = True
 
 # Get our account information.
+api = tradeapi.REST(config.API_KEY, config.API_SECRET, paper=True)
 account = api.get_account()
 
 # Check if our account is restricted from trading.
 if account.trading_blocked:
+    canTrade = False
     print('Account is currently restricted from trading.')
-
+    else
+        
 # Check how much money we can use to open new positions.
 print('${} is available as buying power.'.format(account.buying_power))
