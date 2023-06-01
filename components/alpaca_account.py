5d3493d40cbc9535e65e220d9aaff3be151ae313
SkyShadex <116922168+SkyShadex@users.noreply.github.com>
2023-06-01 12:21:11 -0400

    Update alpaca_account.py

--- components/alpaca_account.py
+++ components/alpaca_account.py
@@ -2,4 +2,4 @@ from flask import Flask, render_template, request
 import alpaca_trade_api as tradeapi
 import config, json, requests
 
-apiClient = TradingClient(confing.API_KEY, config.SECRET_KEY, paper=True)
+api = TradingClient(confing.API_KEY, config.SECRET_KEY, paper=True)
