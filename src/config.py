61161bb6594d34b9f2ddf0cec1c8e7684ef52877
SkyShadex <116922168+SkyShadex@users.noreply.github.com>
2023-06-04 22:37:36 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 # Security
-API_KEY = 'PKN6X66WUS5HI9FJX9UD'
-API_SECRET = 'VJQOrzQFnAxpNlbFYjI4jB1jJwOw097Y3pLhlnMt'
-WEBHOOK_PASSPHRASE = 'dickclickrichshit'
+API_KEY = ''
+API_SECRET = ''
+WEBHOOK_PASSPHRASE = ''
 
 # Strategy Settings
 REWARD = 3
